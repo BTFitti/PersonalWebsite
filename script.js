f382e4e009b4c1bac9def50ad0c578652aa37e1f
BTFitti <bruno.fitti734@live.com>
2025-03-07 16:40:33 -0300

    created the contacts page

---
+++ script.js
@@ -0,0 +1,22 @@
+const inputs = document.querySelectorAll('input');
+const textArea = document.querySelector('textarea');
+
+
+    inputs.forEach(input => {
+      input.addEventListener('focus', () => {
+        const label = input.previousElementSibling
+        label.classList.add('active');
+      });
+      input.addEventListener('blur', () => {
+        const label = input.previousElementSibling
+        label.classList.remove('active');
+      });
+    });
+    textArea.addEventListener('focus', () => {
+      const label = textArea.previousElementSibling
+      label.classList.add('active');
+    });
+    textArea.addEventListener('blur', () => {
+      const label = textArea.previousElementSibling
+      label.classList.remove('active');
+    });
