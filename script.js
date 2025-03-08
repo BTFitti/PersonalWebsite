c774639a7923992910c3573ac42803d3dee1d54e
BTFitti <bruno.fitti734@live.com>
2025-03-08 11:39:06 -0300

    style: added a pending effect in the submit form

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-const inputs = document.querySelectorAll('input');
-const textArea = document.querySelector('textarea');
+const inputs = document.querySelectorAll("input");
+const textArea = document.querySelector("textarea");
 
+inputs.forEach((input) => {
+  input.addEventListener("focus", () => {
+    const label = input.previousElementSibling;
+    label.classList.add("active");
+  });
+  input.addEventListener("blur", () => {
+    const label = input.previousElementSibling;
+    label.classList.remove("active");
+  });
+});
+textArea.addEventListener("focus", () => {
+  const label = textArea.previousElementSibling;
+  label.classList.add("active");
+});
+textArea.addEventListener("blur", () => {
+  const label = textArea.previousElementSibling;
+  label.classList.remove("active");
+});
 
-    inputs.forEach(input => {
-      input.addEventListener('focus', () => {
-        const label = input.previousElementSibling
-        label.classList.add('active');
-      });
-      input.addEventListener('blur', () => {
-        const label = input.previousElementSibling
-        label.classList.remove('active');
-      });
-    });
-    textArea.addEventListener('focus', () => {
-      const label = textArea.previousElementSibling
-      label.classList.add('active');
-    });
-    textArea.addEventListener('blur', () => {
-      const label = textArea.previousElementSibling
-      label.classList.remove('active');
-    });
+document.getElementById("meuForm").addEventListener("submit", function () {
+  document.getElementById("botaoEnviar").disabled = true;
+  document.getElementById("botaoEnviar").textContent = "Sending...";
+});
